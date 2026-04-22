d92673729373a888c1a40068608f355a4bf16449
panchomag8-hue <panchomag8@gmail.com>
2026-04-22 02:27:43 -0500

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,6 +1,3 @@
 export default function handler(req, res) {
-  return res.status(200).json({
-    ok: true,
-    message: "Vercel function works"
-  });
+  return res.status(200).json({ ok: true });
 }
